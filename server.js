44a10869662bacc6fa561e195469f93b5e49de8f
Anthony <anthony.jose53@gmail.com>
2023-01-07 22:15:58 -0500

    the foundation of the bride shop

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+//----------------------------------------
+const app = express();
+
+require('dotenv').config();
+
+//=============================================
+const { PORT = 4000, MONGODB_URL} = process.env;
+
+mongoose.connect(MONGODB_URL);
+
+mongoose.connection
+.on('connected', () => console.log('Connected to MongoDB'))
+.on('error', (err) => console.log('Error with MongoDB: ' + err.message))
+
+
+app.use(cors());
+app.use('/', Routes);
+
+app.get('/', (req, res)=> {
+    res.send("Hello There - Obi_Wan")
+})
